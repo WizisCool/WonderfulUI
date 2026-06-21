d2746c01f5609da031bef90ad46fc99ed0cbbec5
WizisCool <wiziscool@users.noreply.github.com>
2026-06-22 03:59:39 +0800

    fix: use static imports for router views to resolve dynamic/static import conflict

--- packages/gui/src/router/index.ts
+++ packages/gui/src/router/index.ts
@@ -1,22 +1,24 @@
 import { createRouter, createMemoryHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import SettingsView from '../views/SettingsView.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/HomeView.vue'),
+    component: HomeView,
   },
   {
     path: '/match/:id',
     name: 'detail',
-    component: () => import('../views/HomeView.vue'),
+    component: HomeView,
     props: true,
   },
   {
     path: '/settings',
     name: 'settings',
-    component: () => import('../views/SettingsView.vue'),
+    component: SettingsView,
   },
 ];
 
