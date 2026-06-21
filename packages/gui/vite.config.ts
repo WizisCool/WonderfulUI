a95f08369dd307d5459e1fb981b51904b96c0b9d
WizisCool <wiziscool@users.noreply.github.com>
2026-06-21 12:01:20 +0800

    feat(gui): configure vite with vue plugin and @ alias

--- packages/gui/vite.config.ts
+++ packages/gui/vite.config.ts
@@ -1,11 +1,14 @@
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
+  plugins: [vue()],
   root: '.',
   clearScreen: false,
   resolve: {
     alias: {
+      '@': resolve(__dirname, 'src'),
       '@wonderful-ui/parser': resolve(__dirname, '..', 'parser', 'src', 'index.ts'),
     },
   },
